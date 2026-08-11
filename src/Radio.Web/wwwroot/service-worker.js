46436499bb660f762553cbbd51e03f61f0481b56
ignacioCastro0713 <ignaciomenchaca666@gmail.com>
2026-08-11 10:07:50 -0600

    Add PWA support: include manifest and service worker for offline capabilities

---
+++ src/Radio.Web/wwwroot/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "lofi-radio-cache-v1";
+const ASSETS_TO_CACHE = [
+    "/",
+    "/app.css",
+    "/favicon.svg",
+    "/js/lofiPlayer.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
